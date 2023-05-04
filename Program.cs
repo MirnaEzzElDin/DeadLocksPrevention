4c919b7e5b60543914fec839de6d724a24127df9
MirnaEzzElDin <mirnaaezzeldin@gmail.com>
2023-05-04 21:02:55 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeadLocksPrevention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
